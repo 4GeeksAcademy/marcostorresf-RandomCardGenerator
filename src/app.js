c107afbef591682bc0a290b4c6c696d4e46ee59d
marcostorresf <marcos_e19@icloud.com>
2025-03-01 02:04:18 +0000

    almost there

--- src/app.js
+++ src/app.js
@@ -7,5 +7,47 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandomCard()
 };
+
+const generateRandomCard = () => {
+
+let icon = ["♦","♥","♠","♣"];
+let number = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+
+let iconIndex = Math.floor (Math.random() * icon.length)
+let numberIndex = Math.floor(Math.random() * number.length)
+
+let randomIcon = icon[iconIndex]
+let randomNumber = number[numberIndex]
+
+document.querySelector (".top").innerHTML = randomIcon
+document.querySelector(".number").innerHTML = randomNumber
+document.querySelector(".bottom") .innerHTML = randomIcon
+
+
+
+
+
+
+// let suit = icon [Math. floor (Math.random() * icon.length)]; 
+
+// if
+// (suit == "♠" || suit == "♣") {
+//  let suit = element.style.color = 'black';
+// }
+
+// else 
+// (suit == "♥" || suit == "♦") {
+//   let suit = element.style.color = 'red';
+
+// }
+
+
+
+// document.querySelector (".top").innerHTML = icon[Math.floor (Math.random() * icon.length)];
+// document.querySelector (".number").innerHTML = number[Math.floor (Math.random() * number.length)];
+// document.querySelector (".bottom").innerHTML = icon[Math.floor (Math.random() * icon.length)];
+
+
+}
