d6005d8142ae0f859c2cd6ae01b17124ea29a16c
marcostorresf <marcos_e19@icloud.com>
2025-03-04 00:24:23 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -5,49 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  generateRandomCard()
+  document.querySelector("#miBoton").addEventListener("click", generateRandomCard)
+
 };
 
+
 const generateRandomCard = () => {
+const colorChange = document.querySelector(".top")
+const colorChange2 = document.querySelector(".bottom")
+let icon = ["♦", "♥", "♠", "♣"];
+let number = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-let icon = ["♦","♥","♠","♣"];
-let number = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
-
-let iconIndex = Math.floor (Math.random() * icon.length)
+let iconIndex = Math.floor(Math.random() * icon.length)
 let numberIndex = Math.floor(Math.random() * number.length)
 
 let randomIcon = icon[iconIndex]
 let randomNumber = number[numberIndex]
 
-document.querySelector (".top").innerHTML = randomIcon
+if (randomIcon == "♥" || randomIcon == "♦") {
+colorChange.style.color = "red"
+colorChange2.style.color = "red"
+}
+else {
+colorChange.style.color = "black"
+colorChange2.style.color = "black"
+}
+
+colorChange.innerHTML = randomIcon
 document.querySelector(".number").innerHTML = randomNumber
-document.querySelector(".bottom") .innerHTML = randomIcon
-
-
-
-
-
-
-// let suit = icon [Math. floor (Math.random() * icon.length)]; 
-
-// if
-// (suit == "♠" || suit == "♣") {
-//  let suit = element.style.color = 'black';
-// }
-
-// else 
-// (suit == "♥" || suit == "♦") {
-//   let suit = element.style.color = 'red';
-
-// }
-
-
-
-// document.querySelector (".top").innerHTML = icon[Math.floor (Math.random() * icon.length)];
-// document.querySelector (".number").innerHTML = number[Math.floor (Math.random() * number.length)];
-// document.querySelector (".bottom").innerHTML = icon[Math.floor (Math.random() * icon.length)];
-
+colorChange2.innerHTML = randomIcon
 
 }
